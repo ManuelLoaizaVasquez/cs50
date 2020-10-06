e78a9b400790a03b853c6fa72ac1d49e754518a4
ManuelLoaizaVasquez <manulova999@gmail.com>
2020-10-06 17:38:48 -0500

    Implementacion artesanal de cp de Linux.

--- 04_Memory/cp.c
+++ 04_Memory/cp.c
@@ -5,18 +5,28 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
+  // Open input file
   FILE* in = fopen(argv[1], "r");
-  FILE* out = fopen(argv[2], "w");
-
   if (in == NULL) {
+    printf("Could not open %s.\n", argv[1]);
     return 1;
   }
 
+  // Open output file
+  FILE* out = fopen(argv[2], "w");
+  if (out == NULL) {
+    fclose(in);
+    printf("Could not create %s.\n", argv[2]);
+    return 1;
+  }
+
+  // Copy source to destination, one character at a time
   char ch;
   while ((ch = fgetc(in)) != EOF) {
     fputc(ch, out);
   }
 
+  // Close files
   fclose(in);
   fclose(out);
   return 0;
